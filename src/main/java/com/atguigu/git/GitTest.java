4f99035b9f1af251508fdc280f0bc0bd96e0c469
YonghengYu <64342143+yongheng-yu@users.noreply.github.com>
2022-08-04 23:34:16 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("master test1");
         System.out.println("hot-fix");
         System.out.println("push test");
+        System.out.println("pull test");
 
 
 
