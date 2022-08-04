e7450a4210dd7245758eea48753f96b785907b9b
Layne <Layne@atguigu.com>
2022-08-04 22:27:42 +0800

    secong commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,8 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+
+
     }
 }
